68a83e49028ab98bd1eeaf8a4d945e78bc1b419e
Ivan Veselov <veselov.iva@gmail.com>
2017-10-26 19:44:19 +0300

    Add basic reading and tests.

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,14 +1,40 @@
 package ru.spbau.mit
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    
-    words.add("world!")
+fun solve(n: Int, universities: List<Int>, roads: List<Pair<Int, Int>>): Int {
+    return 0
+}
 
-    return words.joinToString(separator = " ")
+class InvalidInputFormatException : Exception {
+    constructor()
+
+    constructor(reason: Exception) : super(reason)
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    fun readLineAsIntList(): List<Int> {
+        val line: String = readLine() ?: throw InvalidInputFormatException()
+
+        try {
+            return line.split(' ').map(String::toInt)
+        } catch(exception: NumberFormatException) {
+            throw InvalidInputFormatException(exception)
+        }
+    }
+
+    fun readLineAsIntPair(): Pair<Int, Int> {
+        val ints: List<Int> = readLineAsIntList()
+
+        if (ints.size != 2) {
+            throw InvalidInputFormatException()
+        }
+
+        val (i1, i2) = ints
+        return Pair(i1, i2)
+    }
+
+    val (n, _) = readLineAsIntPair()
+    val universities: List<Int> = readLineAsIntList()
+    val roads: List<Pair<Int, Int>> = List(n - 1, { readLineAsIntPair() })
+
+    solve(n, universities, roads)
 }
--- src/test/java/ru/spbau/mit/test.kt
+++ src/test/java/ru/spbau/mit/test.kt
@@ -1,10 +1,39 @@
 package ru.spbau.mit
-import kotlin.test.assertEquals
+
 import org.junit.Test
+
+import org.junit.Assert.*
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun test1() {
+        val ans: Int = solve(7,
+                listOf(1, 5, 6, 2),
+                listOf(
+                        Pair(1, 3),
+                        Pair(3, 2),
+                        Pair(4, 5),
+                        Pair(3, 7),
+                        Pair(4, 3),
+                        Pair(4, 6)))
+
+        assertEquals(6, ans)
+    }
+
+    @Test
+    fun test2() {
+        val ans: Int = solve(9,
+                listOf(3, 2, 1, 6, 5, 9),
+                listOf(
+                        Pair(8, 9),
+                        Pair(3, 2),
+                        Pair(2, 7),
+                        Pair(3, 4),
+                        Pair(7, 6),
+                        Pair(4, 5),
+                        Pair(2, 1),
+                        Pair(2, 8)))
+
+        assertEquals(9, ans)
     }
 }
